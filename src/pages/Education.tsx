c71a9e3593f874ad7dbbd6adc36bf1fa9b6618f3
jigneshbhandi09 <jigneshbhandi18@gmail.com>
2025-08-22 11:43:05 +0530

    new changes

--- src/pages/Education.tsx
+++ src/pages/Education.tsx
@@ -23,7 +23,7 @@ const education: Edu[] = [
   {
     degree: "Science",
     institution: "SSS Multipurpose Technical JR College",
-    period: "2018 – 2024",
+    period: "2018 – 2020",
   }
 ];
 
